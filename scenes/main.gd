26ea9d7bd3b7cce8cf05d5f6a8861bc45604d8d4
lerish830-arch <lerish830@gmail.com>
2026-05-31 15:47:57 +0530

    finished all the basic parts
    i finsihed alll the basics set up the collisions and set up the restart part of the game made the high score work now im planning to let alll my imaginations take place and im planning to make some changes

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,3 +1,5 @@
+
+
 extends Node
 
 var stump_scene = preload("res://scenes/stump.tscn")
@@ -16,6 +18,7 @@ const MAX_DIFFICULTY : int = 2
 
 var score : int
 const SCORE_MODIFIER : int = 12
+var high_score : int = 0
 var speed : float
 
 const START_SPEED : float = 10.0
@@ -25,11 +28,13 @@ const SPEED_MODIFIER : int = 5000
 var screen_size : Vector2i
 var ground_height : int
 var game_running : bool
+var game_over_state : bool = false
 var last_obs
 
 func _ready() -> void:
 	screen_size = get_window().size
 	ground_height = $ground.get_node("Sprite2D").texture.get_height()
+	$CanvasLayer.get_node("Button").pressed.connect(new_game)
 	new_game()
 
 func new_game():
@@ -37,13 +42,18 @@ func new_game():
 	show_score()
 	speed = START_SPEED
 	game_running = false
+	game_over_state = false
 	difficulty = 0
 
 	$Dino.position = DINO_START_POS
 	$Dino.velocity = Vector2i(0, 0)
 	$Camera2D.position = CAM_START_POS
 	$ground.position = Vector2i(0, 0)
+
 	$HUD.get_node("startlabel").show()
+	$HUD.get_node("HighScore").text = "HIGH SCORE: " + str(int(high_score / SCORE_MODIFIER))
+
+	$CanvasLayer.hide()
 
 func _process(_delta: float) -> void:
 	if game_running:
@@ -63,12 +73,12 @@ func _process(_delta: float) -> void:
 
 		if $Camera2D.position.x - $ground.position.x > screen_size.x * 1.5:
 			$ground.position.x += screen_size.x
-			
-			
+
 		for obs in obstacles:
 			if obs.position.x < ($Camera2D.position.x - screen_size.x):
 				remove_obs(obs)
-	else:
+
+	elif not game_over_state:
 		if Input.is_action_pressed("ui_accept"):
 			game_running = true
 			$HUD.get_node("startlabel").hide()
@@ -82,25 +92,48 @@ func generate_obs():
 			var obs = obs_type.instantiate()
 			var obs_height = obs.get_node("Sprite2D").texture.get_height()
 			var obs_scale = obs.get_node("Sprite2D").scale
-			var obs_x : int = screen_size.x + score + 100 + (i * 100)
-			var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y / 2) + 5
+
+			var obs_x := int(screen_size.x + score + 100 + (i * 100))
+			var obs_y := int(screen_size.y - ground_height - (obs_height * obs_scale.y / 2.0) + 5)
 
 			last_obs = obs
 			add_obs(obs, obs_x, obs_y)
 
 func add_obs(obs, x, y):
 	obs.position = Vector2i(x, y)
+	obs.body_entered.connect(hit_obs)
 	add_child(obs)
 	obstacles.append(obs)
+
 func remove_obs(obs):
 	obs.queue_free()
-	obstacles .erase(obs)
+	obstacles.erase(obs)
+
+func hit_obs(body):
+	if body.name == "Dino":
+		game_over()
 
 func show_score():
-	$HUD.get_node("Scorelabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
+	$HUD.get_node("Scorelabel").text = "SCORE: " + str(int(score / SCORE_MODIFIER))
+
+func check_high_score():
+	if score > high_score:
+		high_score = score
+
+	$HUD.get_node("HighScore").text = "HIGH SCORE: " + str(int(high_score / SCORE_MODIFIER))
 
 func adjust_difficulty():
-	difficulty = int(score / SPEED_MODIFIER)
+	difficulty = int(score / float(SPEED_MODIFIER))
 
 	if difficulty > MAX_DIFFICULTY:
 		difficulty = MAX_DIFFICULTY
+
+func game_over():
+	game_running = false
+	game_over_state = true
+	speed = 0
+
+	check_high_score()
+
+	print("Game Over")
+	$CanvasLayer.show()
